ddfed14341157136e1dc47deb2db64d3f21792ef
hiro <43287732+h10i@users.noreply.github.com>
2025-03-06 02:30:21 +0000

    Finish C in AtCoder Beginner Contest 389

---
+++ src/abc/abc389/abc389_c.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int q;
+  cin >> q;
+
+  ll top = 0, p = 0;
+  vec<ll> x;
+  rep(i, q) {
+    int query;
+    cin >> query;
+    if (query == 1) {
+      int l;
+      cin >> l;
+      x.push_back(p);
+      p += l;
+    } else if (query == 2) {
+      top++;
+    } else {
+      int k;
+      cin >> k;
+      cout << x[top + k - 1] - x[top] << endl;
+    }
+  }
+  return 0;
+}
