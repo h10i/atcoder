6ec4f2027f510d5e14d74b5dad43323b5322dabe
hiro <43287732+h10i@users.noreply.github.com>
2025-03-07 04:27:30 +0000

    Finish C in AtCoder Beginner Contest 390

---
+++ src/abc/abc390/abc390_c.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int h, w;
+  cin >> h >> w;
+  vec<string> s(h);
+  rep(i, h) cin >> s[i];
+
+  auto f = [&]() -> bool {
+    int h_min = 1000, h_max = -1, w_min = 1000, w_max = -1;
+    rep(i, h) rep(j, w) {
+      if (s[i][j] == '#') {
+        mins(h_min, i);
+        maxs(h_max, i);
+        mins(w_min, j);
+        maxs(w_max, j);
+      }
+    }
+
+    srep(i, h_min, h_max + 1) srep(j, w_min, w_max + 1) {
+      if (s[i][j] == '.') return false;
+    }
+    return true;
+  };
+
+  cout << (f() ? "Yes" : "No") << endl;
+  return 0;
+}
