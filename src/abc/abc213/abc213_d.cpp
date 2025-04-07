9cc1e1887bf8cfeec8a16b88fe49977026569b9f
hiro <43287732+h10i@users.noreply.github.com>
2025-04-07 01:22:21 +0000

    Finish D in AtCoder Beginner Contest 213

---
+++ src/abc/abc213/abc213_d.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  vvi g(n);
+  rep(i, n - 1) {
+    int a, b;
+    cin >> a >> b;
+    a--;
+    b--;
+    g[a].push_back(b);
+    g[b].push_back(a);
+  }
+
+  rep(i, n) sort(rng(g[i]));
+  vi ans;
+  auto dfs = [&](auto f, int v_now, int v_prev) -> void {
+    ans.push_back(v_now);
+    for (int v_next : g[v_now]) {
+      if (v_next == v_prev) continue;
+      f(f, v_next, v_now);
+      ans.push_back(v_now);
+    }
+  };
+
+  dfs(dfs, 0, -1);
+  int ans_size = ans.size();
+  rep(i, ans_size) cout << ans[i] + 1 << (i < ans_size - 1 ? ' ' : '\n');
+  return 0;
+}
