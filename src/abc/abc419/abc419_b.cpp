27d858265e9aea3af659cbf559d9284a0204c380
rano <0x1feb@gmail.com>
2025-09-10 14:03:22 +0000

    Finish B in AtCoder Beginner Contest 419

---
+++ src/abc/abc419/abc419_b.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rrep(i, n) for (int i = 1; i <= (n); ++i)
+#define drep(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define srep(i, s, t) for (int i = s; i < t; ++i)
+#define rng(a) a.begin(), a.end()
+#define rrng(a) a.rbegin(), a.rend()
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+template <typename T>
+using vec = vector<T>;
+template <typename T>
+using vvec = vec<vec<T>>;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+using vi = vec<int>;
+using vvi = vvec<int>;
+const ll LINF = 1001002003004005006ll;
+const int INF = 1001001001;
+const int MOD = 1000000007;
+
+int main() {
+  int q;
+  cin >> q;
+
+  priority_queue<int, vec<int>, greater<int>> que;
+  rep(i, q) {
+    int t;
+    cin >> t;
+    if (t == 1) {
+      int x;
+      cin >> x;
+      que.push(x);
+    } else {
+      int x = que.top();
+      que.pop();
+      cout << x << endl;
+    }
+  }
+  return 0;
+}
